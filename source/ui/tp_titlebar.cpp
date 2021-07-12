cb7a19fef03ef9fbc65678a5dca0d62170a7a342
Zhang Xiang <gbcatmifu@hotmail.com>
2021-07-12 17:31:30 +0800

    Add TP_TitleBar class

---
+++ source/ui/tp_titlebar.cpp
@@ -0,0 +1,38 @@
+﻿#include "tp_titlebar.h"
+
+#include <QMouseEvent>
+
+TP_TitleBar::TP_TitleBar(QWidget *parent) :
+    QFrame(parent),
+    currentPosition(0, 0)
+{
+
+}
+
+// 鼠标相对于窗体的位置 event->globalPos() - this->pos()
+void
+TP_TitleBar::mousePressEvent(QMouseEvent *event)
+{
+    if(event->button() == Qt::LeftButton)
+    {
+        isBeingPressed = true;
+        currentPosition = event->pos();
+    }
+}
+
+// 若鼠标左键被按下，移动窗体位置
+void
+TP_TitleBar::mouseMoveEvent(QMouseEvent *event)
+{
+    if(isBeingPressed)
+        window()->move(event->pos() - currentPosition + pos());
+}
+
+// 设置鼠标未被按下
+void
+TP_TitleBar::mouseReleaseEvent(QMouseEvent *event)
+{
+    Q_UNUSED(event);
+
+    isBeingPressed = false;
+}
---
+++ source/ui/tp_titlebar.h
@@ -0,0 +1,23 @@
+﻿#ifndef TP_TITLEBAR_H
+#define TP_TITLEBAR_H
+
+#include <QFrame>
+
+class QMouseEvent;
+
+class TP_TitleBar : public QFrame
+{
+    Q_OBJECT
+public:
+    TP_TitleBar(QWidget *parent = nullptr);
+
+private:
+    void mousePressEvent(QMouseEvent *event) override;
+    void mouseMoveEvent(QMouseEvent *event) override;
+    void mouseReleaseEvent(QMouseEvent *event) override;
+
+    bool isBeingPressed;
+    QPoint currentPosition;
+};
+
+#endif // TP_TITLEBAR_H
