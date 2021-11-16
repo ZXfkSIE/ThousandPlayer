f51ad7a7ab94a694d33390af4f23ef8808b73503
Zhang Xiang <gbcatmifu@hotmail.com>
2021-11-16 12:18:25 +0800

    change cleanProject.sh a bit

--- cleanProject.sh
+++ cleanProject.sh
@@ -1,1 +1,1 @@
-java -jar ~/bfg-1.14.0.jar --strip-blobs-bigger-than 1M --delete-files '*.{png,svg,ts}' && git reflog expire --expire=now --all && git gc --prune=now --aggressive
+java -jar ~/bfg-1.14.0.jar --strip-blobs-bigger-than 1M --delete-files '*.{png,svg,ts,zip}' && git reflog expire --expire=now --all && git gc --prune=now --aggressive
